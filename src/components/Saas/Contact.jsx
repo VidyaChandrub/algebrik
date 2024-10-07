06c6f4f60fabfbb8fd68ca603a6652dd56b644fc
Vidya <vidhya.c651@gmail.com>
2024-10-07 19:18:10 +0530

    content-changes

--- src/components/Saas/Contact.jsx
+++ src/components/Saas/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
         <h2 className="demo-h2 text-center">Ready to Transform Your Lending Process?</h2>
         <p className="demo-text text-center" >"Get a personalized demo of Algebrik AI to see how it can drive efficiency and scale for your institution."</p>
         <div className="text-center d-grid d-lg-block">
-          <a  className="btn btn-sm hover-blue2 demo-btn px-5 me-lg-5 mb-lg-0 mb-3" href="https://forms.gle/BGmp1RrHHoo2QAQv6" target="_blank" >Get a Demo</a>
+          <a  className="btn btn-sm hover-blue2 demo-btn px-5 me-lg-5 mb-lg-0 mb-3" href="https://forms.gle/BGmp1RrHHoo2QAQv6" >Get a Demo</a>
           <a  className="btn btn-sm hover-blue2 text-white expert-btn " href="tel:+1 917 974 8905">Talk to an Expert</a>
         </div>
         </div>
